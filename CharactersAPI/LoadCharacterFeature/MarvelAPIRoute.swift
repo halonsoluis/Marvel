2c1b7f4a6f8c4162eb51cced900a1485a5430bdd
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-16 06:21:30 +0100

    Assert that urls for endpoints are created as expected

---
+++ CharactersAPI/LoadCharacterFeature/MarvelAPIRoute.swift
@@ -0,0 +1,30 @@
+//
+//  MarvelAPIRoute.swift
+//  CharactersAPI
+//
+//  Created by Hugo Alonso on 16/11/2020.
+//
+
+import Foundation
+
+enum MarvelAPIRoute {
+    ///Fetches lists of characters.
+    case characters
+    ///Fetches a single character by id.
+    case character(id: Int)
+
+    var route: String {
+        "https://gateway.marvel.com:443/v1/public/\(path)"
+    }
+}
+
+extension MarvelAPIRoute {
+    private var path: String {
+        switch self {
+        case .characters:
+            return "characters"
+        case let .character(id):
+            return "characters/\(id)"
+        }
+    }
+}
---
+++ CharactersAPITests/MarvelAPIRouteTests.swift
@@ -0,0 +1,19 @@
+//
+//  MarvelAPIRouteTests.swift
+//  CharactersAPITests
+//
+//  Created by Hugo Alonso on 16/11/2020.
+//
+
+import XCTest
+@testable import CharactersAPI
+
+class MarvelAPIRouteTests: XCTestCase {
+    func testGetRouteListCharacters() {
+        XCTAssertEqual(MarvelAPIRoute.characters.route, "https://gateway.marvel.com:443/v1/public/characters")
+    }
+
+    func testGetRouteFetchSingleCharacter() {
+        XCTAssertEqual(MarvelAPIRoute.character(id: 1).route, "https://gateway.marvel.com:443/v1/public/characters/\(1)")
+    }
+}
