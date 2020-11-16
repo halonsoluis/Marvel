d2e50de6453fdae58a27429a257df746ee308e84
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-16 05:31:52 +0100

    Create the CharacterFeedLoader boundary

---
+++ CharactersAPI/CharactersFeedLoader.swift
@@ -0,0 +1,12 @@
+//
+//  CharactersFeedLoader.swift
+//  CharactersAPI
+//
+//  Created by Hugo Alonso on 16/11/2020.
+//
+
+import Foundation
+
+protocol CharacterFeedLoader {
+    func load(completion: @escaping (Result<MarvelCharacter, Error>) -> Void)
+}
