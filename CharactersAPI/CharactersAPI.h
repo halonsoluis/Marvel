9572575c805e77f1a6a3e98cda3f3047ce5de0dd
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-16 03:44:21 +0100

    Initial Commit

---
+++ CharactersAPI/CharactersAPI.h
@@ -0,0 +1,18 @@
+//
+//  CharactersAPI.h
+//  CharactersAPI
+//
+//  Created by Hugo Alonso on 16/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CharactersAPI.
+FOUNDATION_EXPORT double CharactersAPIVersionNumber;
+
+//! Project version string for CharactersAPI.
+FOUNDATION_EXPORT const unsigned char CharactersAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharactersAPI/PublicHeader.h>
+
+
