1c1f650bfbbd4164da52e1221bae7f9c9a251a83
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-16 09:47:13 +0100

    Remove fields that will not be used

--- CharactersAPI/Models/DataWrapper.swift
+++ CharactersAPI/Models/DataWrapper.swift
@@ -10,9 +10,5 @@ import Foundation
 struct DataWrapper<T: Codable>: Codable {
     let code: Int?
     let status: String?
-    let copyright: String?
-    let attributionText: String?
-    let attributionHTML: String?
-    let etag: String?
     let data: DataContainer<T>?
 }
