175af31cacaa2ede9534ffc4be9247aca906c706
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-17 13:33:07 +0100

    Guarantee that MD5 digester works as intended

--- CharactersAPITests/Endpoint Handling/MarvelURL+MD5DigesterTests.swift
+++ CharactersAPITests/Endpoint Handling/MarvelURL+MD5DigesterTests.swift
@@ -10,5 +10,15 @@ import XCTest
 
 class MarvelURL_MD5DigesterTests: XCTestCase {
 
- 
+    func test_HashCreationWithSingleString() {
+        XCTAssertEqual(MarvelURL.MD5Digester.createHash("1abcd1234"),"ffd275c5130566a2916217b101f26150")
+    }
+
+    func test_HashCreationWithMultipleString() {
+        XCTAssertEqual(MarvelURL.MD5Digester.createHash("1ab","cd1","234"),"ffd275c5130566a2916217b101f26150")
+    }
+
+    func test_HashCreationWithEmptyLine() {
+        XCTAssertNotNil(MarvelURL.MD5Digester.createHash(""))
+    }
 }
