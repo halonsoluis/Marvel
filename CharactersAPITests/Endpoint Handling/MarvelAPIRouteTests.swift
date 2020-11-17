fcc6a0076cf6a7e0cf03caff90552ac1253ceb55
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-17 21:03:42 +0100

    Improve test naming

--- CharactersAPITests/Endpoint Handling/MarvelAPIRouteTests.swift
+++ CharactersAPITests/Endpoint Handling/MarvelAPIRouteTests.swift
@@ -9,12 +9,12 @@ import XCTest
 @testable import CharactersAPI
 
 class MarvelAPIRouteTests: XCTestCase {
-    func testGetRouteListCharacters() {
+    func test_GetRoute_ListCharacters() {
         let sut = RouteComposer(url: URL(string: "https://gateway.marvel.com:443/v1/public/")!)
         XCTAssertEqual(sut.characters(), URL(string: "https://gateway.marvel.com:443/v1/public/characters")!)
     }
 
-    func testGetRouteFetchSingleCharacter() {
+    func test_GetRoute_FetchSingleCharacter() {
         let sut = RouteComposer(url: URL(string: "https://gateway.marvel.com:443/v1/public/")!)
         XCTAssertEqual(sut.character(withId: 1), URL(string: "https://gateway.marvel.com:443/v1/public/characters/1")!)
     }
