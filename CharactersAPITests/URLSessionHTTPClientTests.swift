2e7e04bdafc3595d9d327ba868ff6e009d976706
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-17 12:55:50 +0100

    Resolve MarvelAPI specific API requirements

--- CharactersAPITests/URLSessionHTTPClientTests.swift
+++ CharactersAPITests/URLSessionHTTPClientTests.swift
@@ -97,6 +97,56 @@ class URLSessionHTTPClient {
     }
 }
 
+class MarverlURLTests: XCTestCase {
+    func test_includesMarvelAPIRequirementsInURL() {
+
+        let url = URL(string: "www.url.com")!
+
+        let sut = MarverlURL(
+            url,
+            config: MarverlURL.MarvelAPIConfig(
+                itemsPerPage: 10,
+                privateAPIKey: "privateAPIKey",
+                publicAPIKey: "publicAPIKey"),
+            hashResolver: { $0.joined() }
+        )
+
+        let expectedDate = Date()
+        let expectedDateInString = expectedDate.timeIntervalSinceReferenceDate.description
+
+        let expectedURL = URL(string: "www.url.com?offset=0&ts=\(expectedDateInString)&apikey=publicAPIKey&limit=10&hash=\(expectedDateInString)privateAPIKeypublicAPIKey")!
+
+        let marvelURL = sut.url(for: 0, at: expectedDate)!
+
+        XCTAssertEqual(expectedURL.pathComponents, marvelURL.pathComponents)
+        XCTAssertEqual(expectedURL.baseURL, marvelURL.baseURL)
+    }
+
+    func test_includesMarvelAPIRequirementsInURLforPage1() {
+
+        let url = URL(string: "www.url.com")!
+
+        let sut = MarverlURL(
+            url,
+            config: MarverlURL.MarvelAPIConfig(
+                itemsPerPage: 10,
+                privateAPIKey: "privateAPIKey",
+                publicAPIKey: "publicAPIKey"),
+            hashResolver: { $0.joined() }
+        )
+
+        let expectedDate = Date()
+        let expectedDateInString = expectedDate.timeIntervalSinceReferenceDate.description
+
+        let expectedURL = URL(string: "www.url.com?offset=10&ts=\(expectedDateInString)&apikey=publicAPIKey&limit=10&hash=\(expectedDateInString)privateAPIKeypublicAPIKey")!
+
+        let marvelURL = sut.url(for: 1, at: expectedDate)!
+
+        XCTAssertEqual(expectedURL.pathComponents, marvelURL.pathComponents)
+        XCTAssertEqual(expectedURL.baseURL, marvelURL.baseURL)
+    }
+}
+
 class URLSessionHTTPClientTests: XCTestCase {
 
     func test_load_onFailure_FailsWithError() {
@@ -137,7 +187,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             }
             expect.fulfill()
         }
-        wait(for: [expect], timeout: 30)
+        wait(for: [expect], timeout: 5)
     }
 }
 
