ac4ba5f394369cd11a9288dd153f35f2e53063b2
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-17 21:58:17 +0100

    Refactor LoadDataFromNetworkIntegrationTests making test details more obvious

--- CharactersAPITests/LoadDataFromNetworkIntegrationTests.swift
+++ CharactersAPITests/LoadDataFromNetworkIntegrationTests.swift
@@ -11,73 +11,40 @@ import XCTest
 class LoadDataFromNetworkIntegrationTests: XCTestCase {
 
     func test_load_DataFromNetwork() {
-        let sut = URLSessionHTTPClient()
-        let url = MarvelURL(URL(string: "https://gateway.marvel.com:443/v1/public/characters")!)
-            .url(for: 1)!
+        let (sut, marvelURL) = createSUT( "https://gateway.marvel.com:443/v1/public/characters")
+        let url = marvelURL.url(for: 1)!
 
-        let expect = expectation(description: "Waiting for \(url) to resolve")
-
-        sut.get(from: url) { result in
-            switch result {
-            case let .success((_, response)):
-                XCTAssertEqual(response.statusCode, 200)
-            case let .failure(receivedError as NSError):
-                XCTFail(receivedError.description)
-            }
-            expect.fulfill()
-        }
-        wait(for: [expect], timeout: 5)
+        expectToResolve(url: url, sut: sut, with: 200)
     }
 
     func test_load_DataFromNetworkSingleCharacterEndpoint() {
-        let sut = URLSessionHTTPClient()
-        let url = MarvelURL(URL(string: "https://gateway.marvel.com:443/v1/public/characters/1011334")!)
-            .url()!
+        let (sut, marvelURL) = createSUT( "https://gateway.marvel.com:443/v1/public/characters/1011334")
+        let url = marvelURL.url()!
 
-        let expect = expectation(description: "Waiting for \(url) to resolve")
-
-        sut.get(from: url) { result in
-            switch result {
-            case let .success((_, response)):
-                XCTAssertEqual(response.statusCode, 200)
-            case let .failure(receivedError as NSError):
-                XCTFail(receivedError.description)
-            }
-            expect.fulfill()
-        }
-        wait(for: [expect], timeout: 5)
+        expectToResolve(url: url, sut: sut, with: 200)
     }
 
     func test_load_DataFromNetworkSingleCharacterEndpoint_FailsWith404IfNotFound() {
-        let sut = URLSessionHTTPClient()
-        let url = MarvelURL(URL(string: "https://gateway.marvel.com:443/v1/public/characters/10113346")!)
-            .url()!
+        let (sut, marvelURL) = createSUT( "https://gateway.marvel.com:443/v1/public/characters/10113346")
+        let url = marvelURL.url()!
 
-        let expect = expectation(description: "Waiting for \(url) to resolve")
-
-        sut.get(from: url) { result in
-            switch result {
-            case let .success((_, response)):
-                XCTAssertEqual(response.statusCode, 404)
-            case let .failure(receivedError as NSError):
-                XCTFail(receivedError.description)
-            }
-            expect.fulfill()
-        }
-        wait(for: [expect], timeout: 5)
+        expectToResolve(url: url, sut: sut, with: 404)
     }
 
     func test_load_DataFromNetworkFilteringCharacterEndpoint() {
-        let sut = URLSessionHTTPClient()
-        let url = MarvelURL(URL(string: "https://gateway.marvel.com:443/v1/public/characters")!)
-            .url(nameStartingWith: "a")!
+        let (sut, marvelURL) = createSUT("https://gateway.marvel.com:443/v1/public/characters")
+        let url = marvelURL.url(nameStartingWith: "a")!
 
+        expectToResolve(url: url, sut: sut, with: 200)
+    }
+
+    func expectToResolve(url: URL, sut: HTTPClient, with statusCode: Int, file: StaticString = #filePath, line: UInt = #line) {
         let expect = expectation(description: "Waiting for \(url) to resolve")
 
         sut.get(from: url) { result in
             switch result {
             case let .success((_, response)):
-                XCTAssertEqual(response.statusCode, 200)
+                XCTAssertEqual(response.statusCode, statusCode)
             case let .failure(receivedError as NSError):
                 XCTFail(receivedError.description)
             }
@@ -86,5 +53,10 @@ class LoadDataFromNetworkIntegrationTests: XCTestCase {
         wait(for: [expect], timeout: 5)
     }
 
+    func createSUT(_ urlString: String) -> (HTTPClient, MarvelURL) {
+        let sut = URLSessionHTTPClient()
+        let url = MarvelURL(URL(string: urlString)!, config: .shared, hashResolver: MarvelURL.MD5Digester.createHash, timeProvider: Date.init)
 
+        return (sut, url)
+    }
 }
