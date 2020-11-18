4fc57ccad23ebfca522f43f560671b478acd0801
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-18 18:47:03 +0100

    Assert that MarvelCharactersFeedLoaderTests facade hits the network

---
+++ CharactersAPITests/Public Interface/MarvelCharactersFeedLoaderTests.swift
@@ -0,0 +1,40 @@
+//
+//  MarvelCharactersFeedLoaderTests.swift
+//  CharactersAPITests
+//
+//  Created by Hugo Alonso on 18/11/2020.
+//
+
+import XCTest
+import CharactersAPI
+
+class MarvelCharactersFeedLoaderTests: XCTestCase {
+
+    func test_charactersCall_produces_a_httpRequest() {
+        let client = HTTPClientSpy()
+        let sut = MarvelCharactersFeedLoader(client: client)
+
+        sut.characters(page: 0, completion: { _ in })
+
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+    func test_search_produces_a_httpRequest() {
+        let client = HTTPClientSpy()
+        let sut = MarvelCharactersFeedLoader(client: client)
+
+        sut.search(by: "", in: 0, completion: { _ in })
+
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+    func test_singleCharacter_produces_a_httpRequest() {
+        let client = HTTPClientSpy()
+        let sut = MarvelCharactersFeedLoader(client: client)
+
+        sut.character(id: 0, completion: { _ in })
+
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+}
