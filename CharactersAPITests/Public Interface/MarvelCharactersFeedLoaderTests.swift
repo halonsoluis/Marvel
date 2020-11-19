93e7943987cce81ae74ac0ed90f047a3cdafab29
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-19 01:16:43 +0100

    Extract stub data preparation for reducing duplicity

--- CharactersAPITests/Public Interface/MarvelCharactersFeedLoaderTests.swift
+++ CharactersAPITests/Public Interface/MarvelCharactersFeedLoaderTests.swift
@@ -19,9 +19,7 @@ class MarvelCharactersFeedLoaderTests: XCTestCase {
 
     func test_charactersCallWithValidResponse_producesMarvelItems() {
         let sut = makeSUT()
-        let response = makeValidJSONResponse(amountOfItems: 10)
-        let data: Data = try! JSONSerialization.data(withJSONObject: response.response, options: .prettyPrinted)
-        URLProtocolStub.stub(data: data, response: URLResponse(url: URL(string: "www.anyurl.com")!, mimeType: nil, expectedContentLength: 0, textEncodingName: nil), error: nil)
+        stubHTTPResponseAndData(itemAmount: 10, statusCode: 200)
 
         let expect = expectation(description: "A request for data to the network was issued")
 
@@ -39,9 +37,7 @@ class MarvelCharactersFeedLoaderTests: XCTestCase {
 
     func test_searchWithValidResponse_producesMarvelItems() {
         let sut = makeSUT()
-        let response = makeValidJSONResponse(amountOfItems: 10)
-        let data: Data = try! JSONSerialization.data(withJSONObject: response.response, options: .prettyPrinted)
-        URLProtocolStub.stub(data: data, response: HTTPURLResponse(url: URL(string: "www.anyurl.com")!, statusCode: 200, httpVersion: nil, headerFields: nil), error: nil)
+        stubHTTPResponseAndData(itemAmount: 10, statusCode: 200)
 
         let expect = expectation(description: "A request for data to the network was issued")
 
@@ -59,19 +55,19 @@ class MarvelCharactersFeedLoaderTests: XCTestCase {
 
     func test_singleCharacterWithValidResponse_producesAMarvelItem() {
         let sut = makeSUT()
-        let response = makeValidJSONResponse(amountOfItems: 1)
-        let data: Data = try! JSONSerialization.data(withJSONObject: response.response, options: .prettyPrinted)
-        URLProtocolStub.stub(data: data, response: HTTPURLResponse(url: URL(string: "www.anyurl.com")!, statusCode: 200, httpVersion: nil, headerFields: nil), error: nil)
+        stubHTTPResponseAndData(itemAmount: 1, statusCode: 200)
 
         let expect = expectation(description: "A request for data to the network was issued")
 
         sut.character(id: 0) { result in
             switch result {
             case let .success(item):
-                XCTAssertEqual(item?.name, response.item["name"] as? String)
-                XCTAssertEqual(item?.description, response.item["description"] as? String)
-                XCTAssertEqual(item?.id, response.item["id"] as? Int)
-                XCTAssertEqual(item?.modified, response.item["modified"] as? String)
+                let jsonItem = self.makeValidJSONResponse(amountOfItems: 1, statusCode: 200).item
+
+                XCTAssertEqual(item?.name, jsonItem["name"] as? String)
+                XCTAssertEqual(item?.description, jsonItem["description"] as? String)
+                XCTAssertEqual(item?.id, jsonItem["id"] as? Int)
+                XCTAssertEqual(item?.modified, jsonItem["modified"] as? String)
             default:
                 XCTFail("Unexpected state, expected success received \(result)")
             }
@@ -82,9 +78,7 @@ class MarvelCharactersFeedLoaderTests: XCTestCase {
 
     func test_singleCharacterWithUnknownIdAndValidResponse_doesNotProduceAMarvelItem() {
         let sut = makeSUT()
-        let response = makeValidJSONResponse(amountOfItems: 0)
-        let data: Data = try! JSONSerialization.data(withJSONObject: response.response, options: .prettyPrinted)
-        URLProtocolStub.stub(data: data, response: HTTPURLResponse(url: URL(string: "www.anyurl.com")!, statusCode: 200, httpVersion: nil, headerFields: nil), error: nil)
+        stubHTTPResponseAndData(itemAmount: 0, statusCode: 200)
 
         let expect = expectation(description: "A request for data to the network was issued")
 
@@ -98,6 +92,16 @@ class MarvelCharactersFeedLoaderTests: XCTestCase {
             expect.fulfill()
         }
         wait(for: [expect], timeout: 1.0)
+    }
+
+    private func stubHTTPResponseAndData(itemAmount: Int, statusCode: Int = 200) {
+        let data: Data = try! JSONSerialization.data(
+            withJSONObject: makeValidJSONResponse(amountOfItems: itemAmount, statusCode: statusCode).response,
+            options: .prettyPrinted
+        )
+        let response = HTTPURLResponse(url: URL(string: "www.anyurl.com")!, statusCode: statusCode, httpVersion: nil, headerFields: nil)
+
+        URLProtocolStub.stub(data: data, response: response, error: nil)
     }
 
     private func makeSUT() -> CharacterFeedLoader {
