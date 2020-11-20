048c090e56c9da46d84064a0e7ee237ed731d0e4
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-20 03:03:06 +0100

    Create ImageLoader framework structure

---
+++ ImageLoader/ImageLoader/ImageLoader.h
@@ -0,0 +1,18 @@
+//
+//  ImageLoader.h
+//  ImageLoader
+//
+//  Created by Hugo Alonso on 20/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageLoader.
+FOUNDATION_EXPORT double ImageLoaderVersionNumber;
+
+//! Project version string for ImageLoader.
+FOUNDATION_EXPORT const unsigned char ImageLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageLoader/PublicHeader.h>
+
+
