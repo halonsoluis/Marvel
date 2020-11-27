58a4cd1e39b057c20d51f7ab547b15fd58234e86
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-27 02:04:20 +0100

    Move MarvelFeedProvider & MainComposer to Production

---
+++ Marvel/Marvel/MainComposer.swift
@@ -0,0 +1,29 @@
+//
+//  MainComposer.swift
+//  Marvel
+//
+//  Created by Hugo Alonso on 27/11/2020.
+//
+
+import Foundation
+import UIKit
+import CharactersAPI
+import ImageLoader
+
+class MainComposer {
+
+    func main() {
+        let client = URLSessionHTTPClient(session: URLSession.shared)
+        let charactersLoader = MarvelCharactersFeedLoader(client: client)
+
+        let prefetchImageHandler = { (url: URL, modifiedKey: String) in
+            ImageLoader(url: url, uniqueKey: modifiedKey).image.prefetch(completion: { _ in })
+        }
+
+        let loadImageHandler = { (url: URL, modifiedKey: String, imageView: UIImageView) in
+            ImageLoader(url: url, uniqueKey: modifiedKey).image.render(on: imageView, completion: { _ in })
+        }
+
+        _ = MarvelFeedProvider(charactersLoader: charactersLoader, prefetchImageHandler: prefetchImageHandler, loadImageHandler: loadImageHandler)
+    }
+}
--- Marvel/MarvelTests/MarvelTests.swift
+++ Marvel/MarvelTests/MarvelTests.swift
@@ -8,29 +8,6 @@
 import XCTest
 @testable import Marvel
 
-import UIKit
-import CharactersAPI
-import ImageLoader
-
-class MainComposer {
-
-    func main() {
-        let client = URLSessionHTTPClient(session: URLSession.shared)
-        let charactersLoader = MarvelCharactersFeedLoader(client: client)
-
-        let prefetchImageHandler = { (url: URL, modifiedKey: String) in
-            ImageLoader(url: url, uniqueKey: modifiedKey).image.prefetch(completion: { _ in })
-        }
-
-        let loadImageHandler = { (url: URL, modifiedKey: String, imageView: UIImageView) in
-            ImageLoader(url: url, uniqueKey: modifiedKey).image.render(on: imageView, completion: { _ in })
-        }
-
-        _ = MarvelFeedProvider(charactersLoader: charactersLoader, prefetchImageHandler: prefetchImageHandler, loadImageHandler: loadImageHandler)
-    }
-}
-
-
 class MarvelTests: XCTestCase {
     func test_MainComposerIntegration() {
         let sut = MainComposer()
