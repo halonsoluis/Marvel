24c2810d3d2da3621a92207ed2d0e4d744eba283
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-11-29 01:23:45 +0100

    Adjust List View to design

--- Marvel/Marvel/FeedViewController.swift
+++ Marvel/Marvel/FeedViewController.swift
@@ -6,18 +6,74 @@
 //
 import Foundation
 import UIKit
-
+import SnapKit
 
 
 class ItemCell: UITableViewCell {
 
+    private lazy var name: UIButton = self.createNameButton()
+    private lazy var heroImage: UIImageView = self.createHeroImageView()
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupUI()
+    }
+
+    private func setupUI() {
+        backgroundColor = .clear
+        clipsToBounds = true
+        selectionStyle = .none
+
+        addSubview(heroImage)
+        addSubview(name)
+
+        heroImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        name.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().multipliedBy(1.5)
+            make.right.equalToSuperview().inset(16)
+        }
+    }
+
     func setup(using feedDataProvider: FeedDataProvider, itemAt index: Int) {
-        if let imageField = imageView {
-            feedDataProvider.perform(action: .setHeroImage(index: index, on: imageField))
-        }
-        if let nameField = textLabel {
-            nameField.text = feedDataProvider.items[index].name
-        }
+        feedDataProvider.perform(action: .setHeroImage(index: index, on: heroImage))
+        name.setTitle(feedDataProvider.items[index].name, for: .normal)
+    }
+}
+
+// MARK: Initialisers
+extension ItemCell {
+    private func createHeroImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.accessibilityIdentifier = "heroImage"
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }
+
+    private func createNameButton() -> UIButton {
+        let nameLabel = UIButton()
+        nameLabel.setTitleColor(.black, for: .normal)
+        nameLabel.titleLabel?.adjustsFontSizeToFitWidth = false
+        nameLabel.titleLabel?.autoresizesSubviews = true
+        nameLabel.autoresizingMask = [UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleTopMargin]
+        nameLabel.backgroundColor = .clear
+        nameLabel.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        nameLabel.accessibilityIdentifier = "name"
+        nameLabel.titleLabel?.lineBreakMode = .byWordWrapping
+        nameLabel.titleLabel?.numberOfLines = 0
+        nameLabel.setBackgroundImage(UIImage(named: "bg-cell-title"), for: .normal)
+
+        nameLabel.titleEdgeInsets = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        return nameLabel
     }
 }
 
@@ -54,10 +110,15 @@ class FeedViewController: UITableViewController {
     }
 
     func prepareTableView() {
+        tableView.allowsMultipleSelection = false
+        tableView.keyboardDismissMode = .onDrag
+        tableView.rowHeight = 180
+        tableView.separatorStyle = .none
+        tableView.accessibilityIdentifier = "ItemsTableView"
+        tableView.register(ItemCell.self, forCellReuseIdentifier: "ItemCell")
+
         tableView.delegate = self
         tableView.prefetchDataSource = self
-
-        tableView.register(ItemCell.self, forCellReuseIdentifier: "ItemCell")
 
         tableView.refreshControl = UIRefreshControl()
         tableView.refreshControl?.addTarget(
