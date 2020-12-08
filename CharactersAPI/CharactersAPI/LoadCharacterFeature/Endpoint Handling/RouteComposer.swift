9d13cfd263c0a5d93acfa61ef40a84c0f82d0fe5
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-12-08 03:42:01 +0100

    Add routes for obtaining related publications

--- CharactersAPI/CharactersAPI/LoadCharacterFeature/Endpoint Handling/RouteComposer.swift
+++ CharactersAPI/CharactersAPI/LoadCharacterFeature/Endpoint Handling/RouteComposer.swift
@@ -13,12 +13,46 @@ enum RouteComposer {
     ///Fetches a single character by id.
     case character(id: Int)
 
+    ///Fetches lists of comics filtered by a character id.
+    case comics(characterId: Int)
+
+    ///Fetches lists of events filtered by a character id.
+    case events(characterId: Int)
+
+    ///Fetches lists of series filtered by a character id.
+    case series(characterId: Int)
+
+    ///Fetches lists of stories filtered by a character id.
+    case stories(characterId: Int)
+
     func url(from baseURL: URL) -> URL {
         switch self {
         case .characters:
-            return baseURL.appendingPathComponent("characters")
+            return characters(from: baseURL)
         case .character(let id):
-            return baseURL.appendingPathComponent("characters").appendingPathComponent(id.description)
+            return character(id: id, from: baseURL)
+        case .comics(characterId: let id):
+            return endpoint("comics", id: id, from: baseURL)
+        case .events(characterId: let id):
+            return endpoint("events", id: id, from: baseURL)
+        case .series(characterId: let id):
+            return endpoint("series", id: id, from: baseURL)
+        case .stories(characterId: let id):
+            return endpoint("stories", id: id, from: baseURL)
         }
+    }
+
+    private func characters(from baseURL: URL) -> URL {
+        baseURL.appendingPathComponent("characters")
+    }
+
+    private func character(id: Int, from baseURL: URL) -> URL {
+        characters(from: baseURL)
+            .appendingPathComponent(id.description)
+    }
+
+    private func endpoint(_ endpoint: String, id: Int, from baseURL: URL) -> URL {
+        character(id: id, from: baseURL)
+            .appendingPathComponent(endpoint)
     }
 }
