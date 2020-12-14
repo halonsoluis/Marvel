37de8af1c1ae6a99a8e2ad7c5bc5cda69fec4a84
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2020-12-14 01:21:52 +0100

    Adjust visuals of the Publication cell

--- Marvel/Marvel/PublicationCell.swift
+++ Marvel/Marvel/PublicationCell.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 final class PublicationCell: UICollectionViewCell {
-    static var cellSize: CGSize = CGSize(width: 119, height: 176)
+    static var cellSize: CGSize = CGSize(width: 119, height: 250)
 
     lazy var image: UIImageView = setupImageView()
-    lazy var nameLabel: UILabel = UILabel()
+    lazy var nameLabel: UILabel = createTitleButton()
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) is not supported")
@@ -31,25 +31,50 @@ final class PublicationCell: UICollectionViewCell {
 
         contentView.addSubview(image)
         image.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(8)
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview().inset(4)
+            make.height.greaterThanOrEqualTo(100)
         }
 
         contentView.addSubview(nameLabel)
         nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(image.snp.bottom).offset(2)
             make.bottom.equalToSuperview()
-            make.left.right.equalToSuperview().inset(8)
+            make.left.right.equalToSuperview().inset(2)
         }
 
         needsUpdateConstraints()
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        image.image = nil
+        nameLabel.text = ""
+    }
+
     private func setupImageView() -> UIImageView {
         let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .clear
         imageView.clipsToBounds = true
         imageView.accessibilityIdentifier = "image"
+
         return imageView
+    }
+
+    private func createTitleButton() -> UILabel {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .white
+        nameLabel.backgroundColor = .clear
+        nameLabel.font = UIFont.boldSystemFont(ofSize: UIFont.smallSystemFontSize)
+        nameLabel.accessibilityIdentifier = "title"
+        nameLabel.lineBreakMode = .byTruncatingTail
+        nameLabel.numberOfLines = 0
+        nameLabel.clipsToBounds = true
+        nameLabel.textAlignment = .center
+        
+        return nameLabel
     }
 
 }
